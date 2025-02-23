dce4ef9f91a49fb62d29717a7a3c1482ef04eb9f
Jacob <jacobmpav@gmail.com>
2025-02-23 14:38:54 -0500

    Completed the launch screen so that it resembles the prototype. Currently it is located in App.js, but this should probably be changed later.

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableWithoutFeedback, ImageBackground } from 'react-native'; 
 
 export default function App() {
-  let x = 1;
-  console.log("App Executed");
+  const handlePress = () => console.log("Launch Screen Interaction"); {/* Placeholder Function: Will be changed to allow navigation beyond the home screen. */}
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </SafeAreaView>
+  return ( 
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.fullScreen}>
+        <ImageBackground 
+          source={require('./assets/launch-bg.png')} 
+          style={styles.background} 
+          resizeMode="cover">
+          <SafeAreaView style={styles.container}>
+            <Image source={require('./assets/echolingo-logo-white.png')} />
+            <Text style={styles.titleText}>EchoLingo</Text>
+            <Text style={styles.subtitleText}>Language Learning for All!</Text>
+          </SafeAreaView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
+  fullScreen: {
+    flex: 1, 
+    width: '100%', 
+    height: '100%',
+  },
   container: {
     flex: 1,
-    backgroundColor: "whitesmoke",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 48,
+    color: "white",
+  },
+  subtitleText: {
+    fontSize: 24,
+    color: "white",
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });

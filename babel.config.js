20d104d6c377ed67702ebdfa938e4aef5fd7855f
Aiden A. <aanthony05411@gmail.com>
2025-03-27 11:10:37 -0500

    Update babel.config.js so firebaseConfig.js can properly read environment variables

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,13 @@ export default function(api) {
     api.cache(true);
     return {
       presets: ['babel-preset-expo'],
-      plugins: ['module:react-native-dotenv'],
+      plugins: [
+        ['module:react-native-dotenv', {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        }],
+      ],
     };
-  }
-  
+}

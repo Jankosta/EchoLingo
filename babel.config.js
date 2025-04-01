228311fe38c4c6e5a6fb4d20fbb9d2dac29eca9e
Jacob <jacobmpav@gmail.com>
2025-03-31 19:29:02 -0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 export default function(api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: [
-        ['module:react-native-dotenv', {
-          moduleName: '@env',
-          path: '.env',
-        }],
-      ],
-    };
+  api.cache(true);
+  return {
+    presets: [
+      'babel-preset-expo', 
+      '@babel/preset-env', // Ensure CommonJS support for Node.js
+    ],
+    plugins: [
+      ['module:react-native-dotenv', { moduleName: '@env', path: '.env' }],
+    ],
+  };
 }
